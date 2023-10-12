57869aa15ad722c053f573e69f5b9e6612973b74
gokaybalkaya <130371393+gokaybalkaya@users.noreply.github.com>
2023-10-12 15:05:20 +0000

    poker card generator

--- src/app.js
+++ src/app.js
@@ -1,4 +1,3 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
@@ -6,6 +5,42 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let card = document.getElementById("card");
+  let suits = ["♠", "♣", "♥", "♦"];
+  let cardNumbers = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "K",
+    "Q",
+    "J",
+    "A"
+  ];
+
+  let suitIndex = getRandomNumber(0, 3);
+  let cardNumberIndex = getRandomNumber(0, 12);
+
+  let suit = suits[suitIndex];
+  let cardNumber = cardNumbers[cardNumberIndex];
+
+  card.getElementsByClassName("top-left-icon")[0].textContent = suit;
+  card.getElementsByClassName("bottom-right-icon")[0].textContent = suit;
+  card.getElementsByClassName("number")[0].textContent = cardNumber;
+  if (suit == "♥" || suit == "♦") {
+    card.getElementsByClassName("top-left-icon")[0].style.color = "red";
+    card.getElementsByClassName("bottom-right-icon")[0].style.color = "red";
+  } else {
+    card.getElementsByClassName("top-left-icon")[0].style.color = "black";
+    card.getElementsByClassName("bottom-right-icon")[0].style.color = "black";
+  }
 };
+
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
